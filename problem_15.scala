fc21b9df5a00b4e158045864fa2152b53b0573f0
Alex Rutkowski <alex.rutkowski@gmail.com>
2010-04-19 20:06:18 -0500

    cleaned up problem 15 solution

--- problem_15.scala
+++ problem_15.scala
@@ -15,12 +15,10 @@ def pathCount(max: Int):Long = {
 }
 
 def pathCount(x:Int, y:Int):Long = {
-   if(y== 0 && x== 0) return 0
-   if(y+x == 1) return 1
+   if(y<0 || x<0) return 0
+   if(y+x < 2) return x+y
    if(pathCounts.containsKey(x) && pathCounts.get(x).containsKey(y)) return pathCounts.get(x).get(y)
-   if(y==0) return pathCount(x-1, y)
-   else if(x==0) return pathCount(x, y-1)
-   else return pathCount(x-1, y) + pathCount(x, y-1)
+   return pathCount(x-1, y) + pathCount(x, y-1)
 }
 
 println(pathCount(20))
